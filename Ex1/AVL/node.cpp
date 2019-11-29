07901ed81fa91f224e76ad72c1b1d9ddefdde239
DanielShkolnik <DanielShkolnik@users.noreply.github.com>
2019-11-29 09:14:35 +0200

    new

--- Ex1/AVL/node.cpp
+++ Ex1/AVL/node.cpp
@@ -1,5 +1,13 @@
-//
-// Created by danie on 28/11/2019.
-//
 
 #include "node.h"
+
+void calcHeight(){
+    return this.height;
+}
+void setLeft(const Node& left);
+void setRight(const Node& right);
+
+template <class K, class D>
+int getHeight(){
+    return this.height;
+}
